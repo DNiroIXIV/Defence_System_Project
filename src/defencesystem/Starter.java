ef7362b72f84ee627cdd8698c8d7e5a268bcc699
DNiroIXIV <dnirodhaixiv@gmail.com>
2025-03-30 22:25:28 +0530

    Initial

---
+++ src/defencesystem/Starter.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package defencesystem;
+
+/**
+ *
+ * @author Nirodha
+ */
+public class Starter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
